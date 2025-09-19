a457bbe903157ef90f1a1c4a433c0f8e44ca1303
JesusSanchezMiranda <jesus.sanchez.miranda@vallegrande.edu.pe>
2025-09-19 16:23:46 -0500

    actu image

--- src/app/pages/productos/productos.ts
+++ src/app/pages/productos/productos.ts
@@ -16,7 +16,7 @@ export class Productos implements OnInit {
     this.meta.updateTag({ name: 'description', content: 'Descubre nuestros productos de alta calidad.' });
     this.meta.updateTag({ property: 'og:title', content: 'Productos | Mi Negocio' });
     this.meta.updateTag({ property: 'og:description', content: 'Encuentra variedad y calidad en cada producto.' });
-    this.meta.updateTag({ property: 'og:image', content: 'https://tusitio.com/assets/productos.jpg' });
+    this.meta.updateTag({ property: 'og:image', content: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2018/11/50-regalos-tecnologicos-menos-50-euros.jpg?tf=3840x' });
   }
 
 }
